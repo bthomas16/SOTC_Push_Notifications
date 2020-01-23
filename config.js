216a02439c6d99d345d5afac90f9647ac1cd758d
Brent <brentthomas.c@gmail.com>
2020-01-23 12:31:31 -0700

    progress

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    aws_table_name: 'fruitsTable',
+    aws_local_config: {
+      region: 'local',
+      endpoint: 'http://localhost:8000'
+    },
+    aws_remote_config: {
+      accessKeyId: 'AKIAJUOLUZPTA374VWAA',
+      secretAccessKey: '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',
+      region: 'us-east-2',
+    }
+  };
